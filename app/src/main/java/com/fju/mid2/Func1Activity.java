4ce8431e63de35b0105c5cd6c86fddc0ded4c953
Jason541029 <jtr5494@gmail.com>
2017-12-09 20:06:47 +0800

    新增ROLL按鈕 顯示1~6數字

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -1,13 +1,27 @@
 package com.fju.mid2;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class Func1Activity extends AppCompatActivity {
+Random r = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func1);
     }
-}
+    public  void  roll(View view){
+        int n =r.nextInt(6)+1;
+        new AlertDialog.Builder(this)
+                .setTitle("結果")
+                .setMessage(n+"")
+                .setPositiveButton("OK",null)
+                .show();}
+
+    }
+
